041adaceb11a53d8526760908e0c61ee1249ca7d
oscarvinasco1994 <30811056+oscarvinasco1994@users.noreply.github.com>
2017-08-28 21:51:20 -0500

    Create FiguraGeometrica.java

---
+++ src/estructurasdedatos/cap1/tiposdedatos/FiguraGeometrica.java
@@ -0,0 +1,24 @@
+public class FiguraGeomatrica {
+	private String codigo;
+  	private String tipo;
+	private String Nombre2;
+  
+  public String getCodigo (){
+  return codigo;
+  }
+  public void SetCodigo (String codigo){
+  this.codigo =codigo;
+  }
+  public String getNombre2 (){
+  return nombre;
+  }
+  public void setNombre2 (String nombre2){
+  this.nombre2 =nombre2;
+  }
+  public String getTipo (){
+  return tipo;
+  }
+  public void setTipo (String Tipo){
+  this.tipo =tipo;
+  }
+}
