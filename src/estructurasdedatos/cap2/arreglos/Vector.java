6ed8e4df01b43b8dbfebaa2c70b1251af183b508
oscarvinasco1994 <30811056+oscarvinasco1994@users.noreply.github.com>
2017-09-04 22:35:55 -0500

    Update Vector.java

--- src/estructurasdedatos/cap2/arreglos/Vector.java
+++ src/estructurasdedatos/cap2/arreglos/Vector.java
@@ -1,14 +1,57 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package estructurasdedatos.cap2.arreglos;
 
-/**
- *
- * @author tusk
- */
+
 public class Vector {
-    
+ 
+    private int tam;
+    private String[] vector;
+
+
+    public void crearVector(int n) {        
+        this.tam = n; 
+        this.vector = new String[this.tam]; 
+    }
+    public void settam(int t) {
+        this.tam = t;
+    }
+    public int gettam() {
+        return tam;
+    }   
+    public boolean Elimiar(int t) {  
+        this.vector = null;
+        return true;
+    }
+    public void Insertar(String n, int pos) { 
+        vector[pos] = n;                     
+    }
+    public String[] listar() {
+
+        for (int i = 0; i < tam; i++) {     
+            System.out.println(vector[i]);  
+        }
+        return vector;
+    }
+    public void buscarDato(String dato) {  
+        int i = 0;
+
+        while (i < tam) {                   
+            if (vector[i].equals(dato)) {   
+                System.out.println("el dato esta en la posicion: " + i);  
+            }
+            i++;
+        }
+    } 
+    public void actualizar (int pos, String dato){   
+        if(pos < tam){                             
+            System.out.println("la posicion no existe"); 
+        }else {
+            vector[pos]= dato;                       
+        }
+    } 
+    public void borrarposicion (int pos){            
+        vector[pos]= null;                           
+    }
 }
+
+
