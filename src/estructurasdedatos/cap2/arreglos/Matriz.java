5d5926ae337bb78279857270753e0e09734cae70
oscarvinasco1994 <30811056+oscarvinasco1994@users.noreply.github.com>
2017-09-09 18:01:40 -0500

    Update Matriz.java

--- src/estructurasdedatos/cap2/arreglos/Matriz.java
+++ src/estructurasdedatos/cap2/arreglos/Matriz.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package estructurasdedatos.cap2.arreglos;
 
 /**
@@ -10,5 +5,55 @@ package estructurasdedatos.cap2.arreglos;
  * @author tusk
  */
 public class Matriz {
-    
+   
+//declarar las variables//
+
+    private int filas;           
+    private int columnas;        
+    private int[][] matriz;      
+
+//se le implementa en la estructura matriz//
+    public void crearmatriz(int f, int c) {  
+        this.filas = f;                      
+        this.columnas = c;                  
+        this.matriz = new int[f][c];         
+    }
+
+//se incorporan los datos dentro de la matriz//
+    public void insertar(int f, int c, int dato) {  
+        this.filas = f;
+        this.columnas = c;
+        this.matriz[f][c] = dato;                   
+    }
+
+//eliminar algun dato de la matriz//
+    public void eliminar(int f, int c) {
+        this.filas = f;
+        this.columnas = c;
+        this.matriz[f][c] = 0;
+    }
+
+// para buscar un dato//
+    public void buscardato(int dato) {     
+
+        for (int f = 0; f < matriz.length; f++) {   
+            for (int c = 0; c < matriz.length; c++) {  
+                if (this.matriz[f][c] == dato) {       
+                    System.out.println("el dato se encuenta en la posicion: " + f
+                            + "," + c);
+                }
+            }
+        }
+    }
+
+//  para listar la matriz//
+    public void listar() {
+
+        for (int x = 0; x < matriz.length; x++) {        
+            for (int y = 0; y < matriz[x].length; y++) { 
+                System.out.print(matriz[x][y] + " ");   
+            }
+            System.out.println(" ");
+        }
+    }
 }
