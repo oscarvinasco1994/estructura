11622ae5584215c652c2d1a5ecdad37907e58972
oscarvinasco1994 <30811056+oscarvinasco1994@users.noreply.github.com>
2017-09-11 16:03:30 -0500

    Update NodoSimple.java

--- src/estructurasdedatos/utiles/nodos/NodoSimple.java
+++ src/estructurasdedatos/utiles/nodos/NodoSimple.java
@@ -1,14 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package estructurasdedatos.utiles.nodos;
 
-/**
- *
- * @author tusk
- */
+
 public class NodoSimple {
     
+    private NodoSimple siguiente;
+    private int dato;
+
+    
+    public NodoSimple  getSiguiente() {
+        return siguiente;
+    }
+
+  
+    public void setSiguiente(NodoSimple siguiente) {
+        this.siguiente = siguiente;
+    }
+
+  
+    public int getDato() {
+        return dato;
+    }
+
+ 
+    public void setDato(int dato) {
+        this.dato = dato;
+    }
+
+    public void crearnodo() {
+        this.siguiente = null;
+        this.dato = 0;
+
+    }
+
 }
