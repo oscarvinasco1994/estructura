005a89f0f1344ecb2ab8c80e29b3391ec9b30036
oscarvinasco1994 <30811056+oscarvinasco1994@users.noreply.github.com>
2017-09-16 16:17:59 -0500

    Update ListaSimple.java

--- src/estructurasdedatos/cap2/listas/ListaSimple.java
+++ src/estructurasdedatos/cap2/listas/ListaSimple.java
@@ -1,14 +1,64 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package estructurasdedatos.cap2.listas;
 
-/**
- *
- * @author tusk
- */
+import estructurasdedatos.utiles.nodos.NodoSimple;
+
+
 public class ListaSimple {
-    
-}
+
+    private NodoSimple cabeza;
+    private NodoSimple cola;
+    private NodoSimple actual;
+
+    public void CrearListaSimple() {
+
+        this.cabeza = null;
+        this.cola = null;
+
+    }
+
+    public void insertar(int dato) {
+        NodoSimple nodo = new NodoSimple();
+        nodo.crearnodo();
+        nodo.setDato(dato);
+        if (this.cabeza == null && this.cola == null) {
+
+            this.cabeza = nodo;
+            this.cola = nodo;
+
+        } else {
+            cola.setSiguiente(nodo);
+            this.cola = nodo;
+
+        }
+    }
+
+    public void eliminar(int dato) {
+        if (cabeza.getDato() == dato) {
+            cabeza = cabeza.getSiguiente();
+        } else {
+            NodoSimple aux = cabeza;
+            while (aux.getSiguiente().getDato() != dato) {
+                aux = aux.getSiguiente();
+            }
+            NodoSimple siguiente = aux.getSiguiente().getSiguiente();
+            aux.setSiguiente(siguiente);
+        }
+
+    }
+
+    public void listar() {
+
+        if (!(this.cabeza == null && this.cola == null)) {
+
+            NodoSimple aux = cabeza;
+
+            int i = 0;
+            while (aux != null) {
+             System.out.println("el dato :" + "[" + aux.getDato() + "]" + " " + "esta en la posicion" + "(" + i + ")" + "de la lista");
+              aux = aux.getSiguiente();
+                i++;
+            }
+        }
+
+    }
