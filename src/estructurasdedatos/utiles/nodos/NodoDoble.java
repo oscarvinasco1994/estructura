73f7f971a49aa4d70788f6a388acc9431c8771b7
oscarvinasco1994 <30811056+oscarvinasco1994@users.noreply.github.com>
2017-09-20 23:03:22 -0500

    Update NodoDoble.java

--- src/estructurasdedatos/utiles/nodos/NodoDoble.java
+++ src/estructurasdedatos/utiles/nodos/NodoDoble.java
@@ -1,14 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package estructurasdedatos.utiles.nodos;
 
-/**
- *
- * @author tusk
- */
+
 public class NodoDoble {
-    
+    private int dato;
+    private NodoDoble anterior;
+    private NodoDoble siguiente;
+
+ 
+    public int getDato() {
+        return dato;
+    }
+   
+    public void setDato(int dato) {
+        this.dato = dato;
+    }
+  
+    public NodoDoble getAnterior() {
+        return anterior;
+    }
+   
+    public void setAnterior(NodoDoble anterior) {
+        this.anterior = anterior;
+    }
+ 
+    public NodoDoble getSiguiente() {
+        return siguiente;
+    }
+  
+    public void setSiguiente(NodoDoble siguiente) {
+        this.siguiente = siguiente;
+    }
+    public void crearnodoListadoble (){
+        
+        this.siguiente=null;
+        this.anterior=null;
+        this.dato=0;
+        
+    } 
 }
