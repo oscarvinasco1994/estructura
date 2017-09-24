509fbeef3a9ea77135ddd9898c2fce0a08f21996
oscarvinasco1994 <30811056+oscarvinasco1994@users.noreply.github.com>
2017-09-24 18:51:03 -0500

    Update Pila.java

--- src/estructurasdedatos/cap2/listas/Pila.java
+++ src/estructurasdedatos/cap2/listas/Pila.java
@@ -5,10 +5,6 @@
  */
 package estructurasdedatos.cap2.listas;
 import estructurasdedatos.utiles.nodos.NodoSimple;
-/**
- *
- * @author tusk
- */
 public class Pila {
     
       private Actividad dato;
@@ -16,16 +12,12 @@ public class Pila {
     private NodoSimple tope;
     
     
-     /**
-     * @return the dato
-     */
+    
     public Actividad getDato() {
         return dato;
     }
 
-    /**
-     * @param dato the dato to set
-     */
+
     public void setDato(Actividad dato) {
         this.dato = dato;
     }
@@ -56,14 +48,14 @@ public class Pila {
     }
        
        public void desapilar() {
-        // verificar que la lista este vacia
+       
         if (vacia()) {
-            // si la lista se encuentra vacia imprimos en pantalla
+          
             System.out.println("No existen elementos en la pila");
         } else {
-            // se imprime en pantalla el dato eliminado 
+           
             System.out.println("dato ( " + tope.getDato().getNombre() + " )" + " Ha sido eliminado");
-            // tomar ahora el dato seguiente del eliminado 
+           
             tope = tope.getSiguiente();
 
         }
@@ -71,12 +63,11 @@ public class Pila {
    
        
         public void listar() {
-        // verificar que la pila no este vacia 
+       
         if (!(tope == null)) {
-            // se crea un nodo auxiliar que nos va a recorrer la pila 
-            // imprimir los elementos de la pila
+           
             NodoSimple aux = tope;
-            // ciclo por el cual el nodo recorre la lista hasta que la  pila tenga el valor de null
+            
             System.out.println("LA PILA SE CONFORMA POR");
             while (aux != null) {
 
