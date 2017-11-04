5bf7d2db3fc8b4058bf81c5b80c72c62d3189918
oscarvinasco1994 <30811056+oscarvinasco1994@users.noreply.github.com>
2017-11-03 21:32:57 -0500

    Create Vertice.java

---
+++ src/estructurasdedatos/Vertice.java
@@ -0,0 +1,4 @@
+package grafos;
+/** * * @author utp */public class Vertice {    private String dato;
+    /**     * @return the dato     */    public String getDato() {        return dato;    }
+    /**     * @param dato the dato to set     */    public void setDato(String dato) {        this.dato = dato;    }        public void crearNodo(){    this.dato=null;    }}
