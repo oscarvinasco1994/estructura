61f2938c66586689f2394e49baafcedf41c80e95
oscarvinasco1994 <30811056+oscarvinasco1994@users.noreply.github.com>
2017-11-16 18:58:36 -0500

    Create ArcoNoDirigido.java

---
+++ src/estructurasdedatos/cap5/grafos/ArcoNoDirigido.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estructurasdedatos.cap5.grafos;
+
+public class ArcoNoDirigido {
+    private Vertice verti1;
+    private Vertice verti2;
+
+    /**
+     * @return the verti1
+     */
+    public Vertice getVerti1() {
+        return verti1;
+    }
+
+    /**
+     * @param verti1 the verti1 to set
+     */
+    public void setVerti1(Vertice verti1) {
+        this.verti1 = verti1;
+    }
+
+    /**
+     * @return the verti2
+     */
+    public Vertice getVerti2() {
+        return verti2;
+    }
+
+    /**
+     * @param verti2 the verti2 to set
+     */
+    public void setVerti2(Vertice verti2) {
+        this.verti2 = verti2;
+    }
+    
+}
